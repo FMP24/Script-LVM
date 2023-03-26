43a1752079f835548c70926d895b4d825cc47e95
FMP24 <126695764+FMP24@users.noreply.github.com>
2023-03-26 14:23:25 +0200

    Update LV20GB.sh

--- LV20GB.sh
+++ LV20GB.sh
@@ -9,31 +9,19 @@
 
 # root
 f_comprobarroot
-if [ $? -eq 0 ]; then
 # internet
 f_conexion
-  if [ $? -eq 0 ]; then
 # paquetes
 f_existepaquete
-
-    if [ $? -eq 0 ]; then
 # deteccion discos
 f_comprobar_part
-
-      if [ $? -eq 0 ]; then
 # vg01
 f_crearvg
 #lv20gb
 f_crearlv
-        if [ $? -eq 0 ]; then
 #mkfs
 f_asignarsisfichc
-          if [ $? -eq 0 ]; then
-echo "Operacion completada existosamente"
-          fi
-        fi
-      fi
-    fi
-  fi
+if [ $? -eq 0 ]; then
+  echo "Operacion completada existosamente"
 fi
 
